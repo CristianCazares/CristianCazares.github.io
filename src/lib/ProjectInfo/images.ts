9a1bada1497b477f8e855f13e910cf88689ba55a
CristianCazares <cm_cjavier@hotmail.com>
2023-07-31 19:42:52 -0600

    DELETED PROJECT

--- src/lib/ProjectInfo/images.ts
+++
@@ -1,8 +0,0 @@
-import IBM from "@/../public/img/MockUps/IBMStrategicDashboard/MacBook.png";
-import FoodBank from "@/../public/img/MockUps/FoodBank/mobiles.png";
-import IJALTI from "@/../public/img/MockUps/IJALTIPeers/windows.png";
-import BloodManager from "@/../public/img/MockUps/BloodManager/computer.png";
-import ICOF from "@/../public/img/MockUps/ICOF/vr.png";
-import Multiagents from "@/../public/img/MockUps/Multiagents/illustration.png";
-
-export { IBM, FoodBank, IJALTI, BloodManager, ICOF, Multiagents };
